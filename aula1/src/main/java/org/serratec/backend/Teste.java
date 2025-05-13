6d5b21acf3473d666cf0859bb694f20f43607fbe
lcsvinhas <l.vinhas.lv@gmail.com>
2025-05-12 21:54:39 -0300

    criei aula1

---
+++ aula1/src/main/java/org/serratec/backend/Teste.java
@@ -0,0 +1,20 @@
+package org.serratec.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alunos")
+public class Teste {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping
+    public String oi() {
+        return "Oi Mundo";
+    }
+}
